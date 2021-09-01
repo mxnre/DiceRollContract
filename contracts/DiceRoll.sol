0e604842bd575d47783e63ae2b519c4d14ceac54
felliix <cdev9382@gmail.com>
2021-09-02 02:47:44 +0800

    Update DiceRoll

--- contracts/DiceRoll.sol
+++ contracts/DiceRoll.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "./interfaces/IUnifiedLiquidityPool.sol";
 import "./interfaces/IGembitesProxy.sol";
 import "./interfaces/IRandomNumberGenerator.sol";
@@ -17,7 +16,6 @@ import "./interfaces/IRandomNumberGenerator.sol";
 contract DiceRoll is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
     using Address for address;
-    using Counters for Counters.Counter;
 
     /// @notice Event emitted when gembites proxy set.
     event GembitesProxySet(address newProxyAddress);
@@ -25,16 +23,17 @@ contract DiceRoll is Ownable, ReentrancyGuard {
     /// @notice Event emitted when contract is deployed.
     event DiceRollDeployed();
 
-    event BetPlaced(
+    /// @notice Event emitted when bet is started.
+    event BetStarted(
         address player,
-        uint256 betId,
+        uint256 multiplier,
         uint256 number,
         uint256 amount,
-        uint256 roll
+        bool rollOver
     );
-    event BetSettled(address player, uint256 betId, uint256 betResult); // betResult 1 for win, 0 for loss
 
-    Counters.Counter private betIds;
+    /// @notice Event emitted when bet is finished.
+    event Betfinished(address player, uint256 paidAmount, bool betResult);
 
     IUnifiedLiquidityPool public ULP;
     IERC20 public GBTS;
@@ -44,35 +43,37 @@ contract DiceRoll is Ownable, ReentrancyGuard {
     uint256 public betGBTS;
     uint256 public paidGBTS;
 
+    uint256 public gameId;
+
     struct BetInfo {
         address player;
-        uint256 betId;
         uint256 number;
         uint256 amount;
         uint256 multiplier;
         uint256 expectedWin;
-        uint256 roll; // 1 for over, 0 for under
+        bool rollOver; // true: win when user's number is greater than chainlink random number, false: win when user's number is less than random number.
         bytes32 requestId;
     }
 
     mapping(bytes32 => BetInfo) public requestToBet;
-    mapping(uint256 => BetInfo) public betIdToBet;
-    mapping(uint256 => uint256) public betIdToRoll;
 
     /**
      * @dev Constructor function
      * @param _ULP Interface of ULP
      * @param _GBTS Interface of GBTS
      * @param _RNG Interface of RandomNumberGenerator
+     * @param _gameId Game Id
      */
     constructor(
         IUnifiedLiquidityPool _ULP,
         IERC20 _GBTS,
-        IRandomNumberGenerator _RNG
+        IRandomNumberGenerator _RNG,
+        uint256 _gameId
     ) {
         ULP = _ULP;
         GBTS = _GBTS;
         RNG = _RNG;
+        gameId = _gameId;
 
         emit DiceRollDeployed();
     }
@@ -80,7 +81,7 @@ contract DiceRoll is Ownable, ReentrancyGuard {
     modifier onlyRNG() {
         require(
             msg.sender == address(RNG),
-            "DiceRoll: Caller is not the RandomNumberGenerator."
+            "DiceRoll: Caller is not the RandomNumberGenerator"
         );
         _;
     }
@@ -89,27 +90,28 @@ contract DiceRoll is Ownable, ReentrancyGuard {
      * @dev External function for start betting. This function can be called by anyone.
      * @param _number Tracks Player Selection for UI
      * @param _amount Amount of player betted.
-     * @param _roll roll
+     * @param _rollOver Roll status
      */
     function bet(
         uint256 _number,
         uint256 _amount,
-        uint256 _roll
-    ) external nonReentrant returns (uint256 betId) {
+        bool _rollOver
+    ) external nonReentrant {
         uint256 winChance;
         uint256 expectedWin;
         uint256 multiplier;
-        uint256 minBet;
-        uint256 maxWin;
-        uint256 currentBetId;
+        uint256 minBetAmount;
+        uint256 maxWinAmount;
 
-        minBet = GembitesProxy.getMinBetAmount();
-        maxWin = GBTS.balanceOf(address(ULP)) / 100;
+        minBetAmount = GembitesProxy.getMinBetAmount();
+        maxWinAmount = GBTS.balanceOf(address(ULP)) / 100;
 
-        require(_roll == 0 || _roll == 1, "DiceRoll: Invalid roll");
-        require(_number > 0 && _number < 50, "DiceRoll: Invalid number");
+        require(
+            _number > 0 && _number < 100,
+            "DiceRoll: Bet amount is out of range"
+        );
 
-        if (_roll == 0) {
+        if (_rollOver) {
             winChance = _number;
         } else {
             winChance = 100 - _number;
@@ -119,43 +121,27 @@ contract DiceRoll is Ownable, ReentrancyGuard {
         expectedWin = (multiplier * _amount) / 1000;
 
         require(
-            _amount >= minBet && expectedWin <= maxWin,
-            "DiceRoll: Amount invalid"
+            _amount >= minBetAmount && expectedWin <= maxWinAmount,
+            "DiceRoll: Expected paid amount is out of range"
         );
 
         GBTS.safeTransferFrom(msg.sender, address(ULP), _amount);
 
         bytes32 requestId = RNG.requestRandomNumber();
 
-        betIds.increment();
-        currentBetId = betIds.current();
-
         requestToBet[requestId] = BetInfo(
             msg.sender,
-            currentBetId,
             _number,
             multiplier,
             _amount,
             expectedWin,
-            _roll,
+            _rollOver,
             requestId
         );
-        betIdToBet[currentBetId] = BetInfo(
-            msg.sender,
-            currentBetId,
-            _number,
-            _amount,
-            multiplier,
-            expectedWin,
-            _roll,
-            requestId
-        );
-
-        emit BetPlaced(msg.sender, currentBetId, _number, _amount, _roll);
 
         betGBTS += _amount;
 
-        return currentBetId;
+        emit BetStarted(msg.sender, multiplier, _number, _amount, _rollOver);
     }
 
     /**
@@ -167,32 +153,21 @@ contract DiceRoll is Ownable, ReentrancyGuard {
         BetInfo memory betInfo = requestToBet[_requestId];
 
         address player = betInfo.player;
-        uint256 rng = _randomness;
-        uint256 betId = betInfo.betId;
-        uint256 number = betInfo.number;
         uint256 expectedWin = betInfo.expectedWin;
-        uint256 roll = betInfo.roll;
 
-        uint256 result = (rng % 100) + 1;
+        uint256 gameNumber = (uint256(
+            keccak256(abi.encode(_randomness, player, gameId))
+        ) % 100) + 1;
 
-        betIdToRoll[betId] = result;
-
-        if (roll == 0) {
-            if (number >= result) {
-                ULP.sendPrize(player, expectedWin);
-                emit BetSettled(player, betId, 1);
-                paidGBTS += expectedWin;
-            } else {
-                emit BetSettled(player, betId, 0);
-            }
-        } else if (roll == 1) {
-            if (number <= result) {
-                ULP.sendPrize(player, expectedWin);
-                emit BetSettled(player, betId, 1);
-                paidGBTS += expectedWin;
-            } else {
-                emit BetSettled(player, betId, 0);
-            }
+        if (
+            (betInfo.rollOver && betInfo.number >= gameNumber) ||
+            (!betInfo.rollOver && betInfo.number <= gameNumber)
+        ) {
+            ULP.sendPrize(player, expectedWin);
+            paidGBTS += expectedWin;
+            emit Betfinished(player, expectedWin, true);
+        } else {
+            emit Betfinished(player, 0, false);
         }
     }
 
@@ -203,7 +178,7 @@ contract DiceRoll is Ownable, ReentrancyGuard {
     function setGembitesProxy(address _newProxyAddress) external onlyOwner {
         require(
             _newProxyAddress.isContract() == true,
-            "CoinFlip: Address is not contract address"
+            "DiceRoll: Address is not contract address"
         );
         GembitesProxy = IGembitesProxy(_newProxyAddress);
 
--- contracts/interfaces/IGame.sol
+++ contracts/interfaces/IGame.sol
@@ -5,5 +5,10 @@ pragma solidity ^0.8.6;
  * @title Game interface
  */
 interface IGame {
+    /**
+     * @dev External function for playing. This function can be called by only RandomNumberGenerator.
+     * @param _requestId Request Id
+     * @param _randomness Random Number
+     */
     function play(bytes32 _requestId, uint256 _randomness) external;
 }
