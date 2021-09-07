a5a407e698814d23a3ea42ae1205362e710f84ae
felliix <cdev9382@gmail.com>
2021-09-07 21:34:12 +0800

    Update migration

--- migrations/6_deploy_diceroll.js
+++ migrations/6_deploy_diceroll.js
@@ -1,14 +1,12 @@
 const DiceRoll = artifacts.require('DiceRoll');
 const UnifiedLiquidityPool = artifacts.require("UnifiedLiquidityPool");
 const GBTS = artifacts.require("GBTS");
-const GembitesProxy = artifacts.require("GembitesProxy");
 const RandomNumberGenerator = artifacts.require("RandomNumberGenerator");
 
 module.exports = async function (deployer) {
 
     GBTS_instance = await GBTS.deployed();
     ULP_instance = await UnifiedLiquidityPool.deployed();
-    Proxy_instance = await GembitesProxy.deployed();
     RNG_instance = await RandomNumberGenerator.deployed();
 
     await deployer.deploy(
@@ -16,7 +14,6 @@ module.exports = async function (deployer) {
         ULP_instance.address, // Deployed ULP Address
         GBTS_instance.address, // Deployed GBTS Address
         RNG_instance.address, // // Deployed RNG Address
-        2, // Game Id
     );
 
     return;
